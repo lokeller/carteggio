468c44e732c9c3ba9704dd3e9801172f4f800ae4
Lorenzo Keller <lorenzo.keller@gmail.com>
2014-07-05 13:16:00 +0200

    Documented MessageTransport interface.

--- Carteggio/src/ch/carteggio/net/MessageTransport.java
+++ Carteggio/src/ch/carteggio/net/MessageTransport.java
@@ -16,14 +16,58 @@ import org.apache.james.mime4j.dom.Message;
 
 import ch.carteggio.provider.CarteggioAccount;
 
+/**
+ * 
+ * This interface defines an object used to send messages.
+ * 
+ * Design considerations: This API could allow Carteggio to support different
+ * types of server protocols (such as SMTP, Exchange, etc. ) but has been
+ * introduced mainly to allow implementation of a mock {@link MessageTransport}
+ * that is used for testing of the components that use this this API without
+ * having to setup an SMTP server.
+ * 
+ * 
+ */
+
 public interface MessageTransport {
 
-	public void sendMessage(Message message) throws MessagingException ;	
-	
+	/**
+	 * 
+	 * Sends the given message.
+	 * 
+	 * @param message
+	 *            a message to be sent
+	 * 
+	 * @throws MessagingException
+	 */
+	public void sendMessage(Message message) throws MessagingException;
+
+	/**
+	 * 
+	 * Factory used to create a message transport for a given
+	 * {@link CarteggioAccount}
+	 * 
+	 * Design considerations: the factory takes as argument an account object
+	 * instead of just a URL because it will probably be necessary to add more
+	 * preferences to a given account (such as enabling workarounds for specific
+	 * server implementations or enabling certificate based logins). Encoding
+	 * all of this inside a URL and parsing it back is cumbersome. Another
+	 * immediate advantage is that we can keep URI and password separated.
+	 * 
+	 */
 	public interface Factory {
-	
-		public MessageTransport getMessageTransport(CarteggioAccount account) throws MessagingException;		
-		
+
+		/**
+		 * 
+		 * Creates a new {@link MessageTransport} for the given account.
+		 * 
+		 * @param account
+		 * @return
+		 * @throws MessagingException
+		 */
+		public MessageTransport getMessageTransport(CarteggioAccount account)
+				throws MessagingException;
+
 	}
-	
+
 }
