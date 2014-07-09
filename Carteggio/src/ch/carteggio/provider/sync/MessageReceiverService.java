a8591d397c36c301740cc32c9679446d9192fad3
Lorenzo Keller <lorenzo.keller@gmail.com>
2014-07-09 08:04:01 +0200

    Code cleanup for MessageReceiverService

--- Carteggio/src/ch/carteggio/provider/sync/MessageReceiverService.java
+++ Carteggio/src/ch/carteggio/provider/sync/MessageReceiverService.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -26,13 +27,60 @@ import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.util.Log;
 import ch.carteggio.net.MessageStore;
-import ch.carteggio.net.MessagingException;
 import ch.carteggio.net.NetworkFactories;
 import ch.carteggio.net.MessageStore.Folder;
 import ch.carteggio.net.MessageStore.SynchronizationPoint;
 import ch.carteggio.provider.AuthenticatorService;
 import ch.carteggio.provider.CarteggioAccount;
 import ch.carteggio.provider.CarteggioAccountImpl;
+
+/**
+ * 
+ * 
+ * This service is responsible to find incoming messages in the
+ * inbox folders of all accounts and store them in the database.
+ * 
+ * The service maintains a connection to each server, listens for
+ * changes from the servers (if supported by the server) and polls
+ * for new messages if requested explicitly by an intent with
+ * action {@link #ACTION_POLL}.
+ * 
+ * Internally when the service starts a {@link MessageReceiver} Thread 
+ * is started for each account. This thread performs the IO with the server and 
+ * can therefore block on read/writes/connections.
+ * 
+ * The thread can be in three states: "unconnected", "connected
+ * waiting for notifications from the server" or "connected and 
+ * sleeping".
+ * 
+ * On service start the thread is in the "unconnected" state, it
+ * starts a connection to the server and if successful it checks
+ * for incoming mails. If the server supports notifications of new
+ * messages the service it goes to the state "connected waiting for
+ * notifications from the server", otherwise it moves to the 
+ * "connected and sleeping state".
+ * 
+ * If at any point there is a connection error the thread moves 
+ * to the "unconnected state"  
+ * 
+ * When the thread is in the "connected waiting for notification from
+ * the server" and the server notifies changes the thread checks
+ * for new emails and goes back to the same state. 
+ * 
+ * When the service receives an intent with action equal to 
+ * {@link #ACTION_POLL}. One of the following happens:
+ * 
+ * <ul>
+ * <li>If the thread is in the "unconnected" state it retries 
+ * to connect to the server, and if successful moves to the appropriate
+ * state as at service start.</li>
+ * <li>If the thread is in the "connected and sleeping" state it 
+ * checks for new messages and goes back to the same state</li>
+ * <li>If the thread is in the "connected waiting for notification
+ * from server" state it ignores the intent.
+ * </ul>
+ * 
+ */
 
 public class MessageReceiverService extends Service {
 	
@@ -42,15 +90,11 @@ public class MessageReceiverService extends Service {
 	
 	public static final String ACTION_POLL = "ch.carteggio.provider.sync.MessageReceiverService.ACTION_POLL";	
 	
-	private WakeLock mWakeLock;
 	
 	@Override
 	public void onCreate() {
 
 		super.onCreate();
-		
-		
-		mWakeLock = ((PowerManager) getSystemService(POWER_SERVICE)).newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MessageReceiverLock");
 		
 		AccountManager manager = AccountManager.get(this);		
 		
@@ -65,8 +109,6 @@ public class MessageReceiverService extends Service {
 			mAccountReceiver.add(accountReceiver);
 			
 		}
-		
-		
 		
 	}
 
@@ -106,17 +148,29 @@ public class MessageReceiverService extends Service {
 		private IncomingMessagesProcessor mProcessor;		
 		
 		private MessageStore mStore;
+	
+		private WakeLock mWakeLock;
 		
 		// design consideration: we keep the reference for the folder
 		// here to be able to close it when we receive the shutdown call
 		private Folder mFolder = null;
 		
+		private String getLogTag() {
+			return LOG_TAG + "/" + mAccount.getEmail();
+		}
+		
 		private MessageReceiver(CarteggioAccount account) {
+
 			this.mAccount = account;						
+			
+			PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+			
+			mWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, getLogTag());
+			
 		}
 		
 		public void run() {
-						
+	
 			mWakeLock.acquire();
 			
 			try {								
@@ -151,7 +205,7 @@ public class MessageReceiverService extends Service {
 							
 			} catch ( Exception ex) {
 
-				Log.e(LOG_TAG, "Error while receiving messages", ex);				
+				Log.e(getLogTag(), "Error while receiving messages", ex);				
 
 				NotificationService.setReceivingError(getApplicationContext(), true);
 							
@@ -164,6 +218,17 @@ public class MessageReceiverService extends Service {
 			
 		}
 
+		public synchronized void poll() {
+			Log.d(getLogTag(), "Poll requested");
+			notifyAll();
+		}
+
+		public void shutdown() {
+			interrupt();
+			try { mFolder.close(); } catch (Exception e2) {}
+		}
+
+		@SuppressLint("Wakelock")
 		private void checkMessages() {
 			
 			try {
@@ -184,24 +249,24 @@ public class MessageReceiverService extends Service {
 						
 						mProcessor.processFolder(mFolder);
 						
-						Log.d(LOG_TAG, "Starting waiting for messages");
+						Log.d(getLogTag(), "Starting waiting for messages");
 						
 						// we get the sync point but we will not save it (we save it only after processing the messages)
 						SynchronizationPoint syncPoint = mStore.createSynchronizationPoint(mAccount.getPushState());
 						mFolder.waitForChanges(syncPoint, mWakeLock);
 				
-						Log.d(LOG_TAG, "Folder has changed, processing new messages");
+						Log.d(getLogTag(), "Folder has changed, processing new messages");
 						
 					}
 					
 				} else {
 									
 					while (!interrupted()) {
-
+		
 						mProcessor.processFolder(mFolder);
-
-						Log.d(LOG_TAG, "Starting waiting for messages");
-
+		
+						Log.d(getLogTag(), "Starting waiting for messages");
+		
 						// sleep until the next time we a requested to check emails
 						// while we wait for incoming messages we don't hold the wake lock
 						mWakeLock.release();
@@ -209,10 +274,10 @@ public class MessageReceiverService extends Service {
 						synchronized (this) {
 							wait();	
 						}
-	
+		
 						mWakeLock.acquire();
-
-						Log.d(LOG_TAG, "Received poll request, checking for new messages");
+		
+						Log.d(getLogTag(), "Received poll request, checking for new messages");
 					
 					}
 					
@@ -220,38 +285,22 @@ public class MessageReceiverService extends Service {
 			
 			} catch (Exception e) {
 				
-				Log.e(LOG_TAG, "Error while polling folder", e);
+				// make sure when we are holding the wake lock
+				if ( !mWakeLock.isHeld()) mWakeLock.acquire();
+				
+				Log.e(getLogTag(), "Error while polling folder", e);
 				
 				if ( mFolder != null) {
 					try { mFolder.close(); } catch (Exception e2) {}
 					mFolder = null;
 				}
 				
-				if ( !mWakeLock.isHeld()) mWakeLock.acquire();
-				
 				NotificationService.setReceivingError(getApplicationContext(), true);
 										
 			} 
 			
 		}
-		
-		public synchronized void poll() {
-			
-			Log.d(LOG_TAG, "Poll requested");
-
-			notifyAll();			
-		}
-
-		public void shutdown() {
-
-			interrupt();
-			
-			try { mFolder.close(); } catch (Exception e2) {}
-			
-		}
-				
 	
 	}
-
 	
 }
