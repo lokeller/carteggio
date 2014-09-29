4952d987a915d0eea1243442a0982dcf55ebdc98
Lorenzo Keller <lorenzo.keller@gmail.com>
2014-09-29 08:20:06 +0200

    Fixed some problems with the schema SQL
    Some bugs slipped in during the switch to external files.

--- Carteggio/assets/schema/1/006_conversations_view.sql
+++ Carteggio/assets/schema/1/006_conversations_view.sql
@@ -1,15 +1,15 @@
 CREATE VIEW view_conversations AS
-	SELECT	conversations._id			AS _id,
+	SELECT	conv._id			AS _id,
+			conv.subject				AS subject,
+			conv.participants_count		AS participants_count,
+			conv.participants_names		AS participants_names,
+			conv.unread_messages_count	AS unread_messages_count,
 			contacts.email				AS last_message_sender_email,
 			contacts.name				AS last_message_sender_name,
 			messages._id				AS last_message_id,
 			messages.state				AS last_message_state,
 			messages.sent_date			AS last_sent_date,
-			messages.text				AS last_message_text,
-			conv.subject				AS subject,
-			conv.participants_count		AS participants_count,
-			conv.participants_names		AS participants_names,
-			conv.unread_messages_count	AS unread_messages_count
+			messages.text				AS last_message_text
 	FROM	conversations AS conv 
 	LEFT JOIN messages ON messages._id = conv.last_message_id 
 	LEFT JOIN contacts ON messages.sender_id = contacts._id;
--- Carteggio/assets/schema/1/012_on_update_message.sql
+++ Carteggio/assets/schema/1/012_on_update_message.sql
@@ -7,7 +7,7 @@ BEGIN
 			FROM	messages
 			WHERE	conversation_id = NEW.conversation_id AND
 				messages.state = 4
-		
+		)
 	 WHERE	_id = NEW.conversation_id;
 END;
 
